c3ac60481b3deaff2b0e4c76e020f9f72fa9bcc5
Yoshihiko Kato <raiden000discord@gmail.ocm>
2020-08-21 14:30:34 +0900

    test

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,40 @@ use std::process::Command;
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let input = &args[1];
+
+    let add_result = add();
+    println!("{}", add_result);
+
+    let commit_result = commit(input);
+    println!("{}", commit_result);
+
+    let push_result = push();
+    println!("{}", push_result);
+}
+
+fn add() -> String {
     let output = Command::new("git")
         .arg("add")
         .arg("-A")
         .output()
         .expect("failed to execute process");
-    let hello = output.stdout;
-    println!("{}", std::str::from_utf8(&hello).unwrap());
+    std::str::from_utf8(&output.stdout).unwrap().to_string()
+}
+
+fn commit(input: &str) -> String {
     let output = Command::new("git")
         .arg("commit")
         .arg("-m")
         .arg(input)
         .output()
         .expect("failed to execute process");
-    let hello = output.stdout;
-    println!("{}", std::str::from_utf8(&hello).unwrap());
+    std::str::from_utf8(&output.stdout).unwrap().to_string()
+}
+
+fn push() -> String {
     let output = Command::new("git")
-    .arg("push")
-    .output()
-    .expect("failed to execute process");
-    let hello = output.stdout;
-    println!("{}", std::str::from_utf8(&hello).unwrap());
+        .arg("push")
+        .output()
+        .expect("failed to execute process");
+    std::str::from_utf8(&output.stdout).unwrap().to_string()
 }
